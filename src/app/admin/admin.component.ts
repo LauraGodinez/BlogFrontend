1b6190c03ad11cb9dc7599ae2aa94dce716227fe
issagodinez <lauragodinez45@gmail.com>
2020-09-29 20:13:00 -0400

    update

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Entries } from "../models/entries";
+import { HttpClient } from "@angular/common/http";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  selector: "app-admin",
+  templateUrl: "./admin.component.html",
+  styleUrls: ["./admin.component.scss"]
 })
 export class AdminComponent implements OnInit {
+  createFormGroup: FormGroup = new FormGroup({
+    title: new FormControl("", Validators.required),
+    content: new FormControl("", Validators.required)
+  });
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitForm(): void {
+    console.log(this.createFormGroup.value);
   }
-
 }
